7142b601119d4fca1769008f879092ff10db1292
Sergio Abreu García <saggamedeveloper@gmail.com>
2019-02-25 02:18:11 +0100

    Work overlay structure added

--- src/components/sections/work.js
+++ src/components/sections/work.js
@@ -3,52 +3,82 @@ import PropTypes from 'prop-types'
 import { withPrefix } from 'gatsby'
 
 import Arrow from '../../components/arrow'
+import cross from '../../images/cross.svg'
 
 class Work extends Component {
 
   componentDidMount() {
     this.work_elements = document.getElementsByClassName("work-element");
-    this.activeZoom = false;
+    this.work_overlay = document.querySelector(".work-overlay");
+    this.overlay_cross = this.work_overlay.querySelector(".work-overlay-cross");
+    this.overlay_active = false;
+
+    // Set the onclick events
     Array.prototype.forEach.call(this.work_elements, (element) => (element.onclick = this.zoomElement.bind(this, element)));
+    this.work_overlay.onclick = this.clickOutsideOverlay.bind(this);
+    this.overlay_cross.onclick = this.setOverlayActive.bind(this, false);
   }
 
   zoomElement(element) {
-    if (element.classList.contains("work-element-zoom")) {
-      element.classList.remove("work-element-zoom");
-      this.activeZoom = false;
+    if (!this.overlay_active) {
+      this.setOverlayActive(true);
     }
-    else if (!this.activeZoom) {
-      element.classList.add("work-element-zoom");
-      this.activeZoom = true;
+  }
+
+  clickOutsideOverlay(e) {
+    // Close the overlay when directly clicking on it
+    // and not on its children, that means cliking the background
+    if (e.target === this.work_overlay) {
+      this.setOverlayActive(false);
+    }
+  }
+
+  setOverlayActive(active) {
+    if (!this.overlay_active && active){
+      this.work_overlay.classList.add("work-overlay-active");
+      this.overlay_active = true;
+    }
+    else if (this.overlay_active) {
+      this.work_overlay.classList.remove("work-overlay-active");
+      this.overlay_active = false;
     }
   }
 
   render() {
     return (
-      <div data-aos="fade-left" id="work-section" className = "container work">
-        <hr/>
-        <div className="section-title">
-          <h1><span className="section-title-capital">W</span>hat I've done</h1>
-        </div>
-        <div className="work-container">
-          { this.props.files.filter(file=>file.node.frontmatter.tag==="work").map(file => {
-            return (
+      <>
+        <div className="work-overlay">
+          <div className="work-overlay-cross"><img alt="cross" src={ cross } /></div>
 
-                <div data-aos="fade-left" key={ file.node.frontmatter.title } className="work-element-wrapper">
-                  <div className="work-element">
-                    <div className="work-element-content" dangerouslySetInnerHTML = {{ __html: file.node.html }} />
-                    <div className="work-element-image">
-                      <img alt={ file.node.frontmatter.title }  src={withPrefix('/images/work/'+file.node.frontmatter.title+'.png')} />
+          <div className="work-overlay-wrapper">
+          </div>
+        </div>
+
+        <div data-aos="fade-left" id="work-section" className = "container work">
+          <hr/>
+          <div className="section-title">
+            <h1><span className="section-title-capital">W</span>hat I've done</h1>
+          </div>
+          <div className="work-container">
+            { this.props.files.filter(file=>file.node.frontmatter.tag==="work").map(file => {
+              return (
+
+                  <div data-aos="fade-left" key={ file.node.frontmatter.title } className="work-element-wrapper">
+                    <div className="work-element">
+                      <div className="work-element-content" dangerouslySetInnerHTML = {{ __html: file.node.html }} />
+                      <div className="work-element-image">
+                        <img alt={ file.node.frontmatter.title }  src={withPrefix('/images/work/'+file.node.frontmatter.title+'.png')} />
+                      </div>
                     </div>
                   </div>
-                </div>
 
-            );
-          }) }
+              );
+            }) }
+          </div>
+
+          <Arrow target_id="skills-section"/>
         </div>
-
-        <Arrow target_id="skills-section"/>
-      </div>
+      </>
     );
   }
 }
