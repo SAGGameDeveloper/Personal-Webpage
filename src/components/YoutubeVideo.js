553f1de3041fbca7f194af719feb1cb4fae2a60d
Sergio Abreu García <saggamedeveloper@gmail.com>
2021-02-16 16:46:56 +0100

    Youtube feeded bugfix & static CNAME added

--- src/components/YoutubeVideo.js
+++ src/components/YoutubeVideo.js
@@ -4,7 +4,7 @@ export default class YoutubeVideo extends Component {
   render() {
     return (
       <div className = "iframe-container">
-        <iframe title="youtube" className="responsive-iframe" src="http://www.youtube.com/embed/videoseries?list=UUUddl_zx5SHrn4csHj_r4xQ" frameBorder="0" allowFullScreen></iframe>
+        <iframe title="youtube" className="responsive-iframe" src="https://www.youtube.com/embed/videoseries?list=UUUddl_zx5SHrn4csHj_r4xQ" frameBorder="0" allowFullScreen></iframe>
       </div>
     );
   }
