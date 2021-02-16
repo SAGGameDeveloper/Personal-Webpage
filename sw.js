d2bcbf3779d211c824a079319037ea37c113bf07
Sergio Abreu García <saggamedeveloper@gmail.com>
2021-02-16 16:45:52 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-65dc29dcba26c75cb9b6.js"
+    "url": "webpack-runtime-06464237fc121e1fbd44.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bcfa16d5811ed8936708a60dd18a9864"
+    "revision": "d1e015245ac311f748307f4c4f92b760"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1fd9c05d1221a6d94ee0fe196b7cc25e"
+    "revision": "99185d21da9395f1832a7d3308eec893"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
